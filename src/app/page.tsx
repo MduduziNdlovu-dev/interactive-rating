db29965ee578ba8037437381bc121bb7b97c1897
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2025-03-28 15:40:00 +0200

    Remove unused SVG files and update Home component structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Box } from "@mui/material";
 
 export default function Home() {
   return (
-    <Box></Box>
+    <Box>
+      
+    </Box>
   );
 }
