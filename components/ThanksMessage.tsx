689538da368fc69003777075c78bb6f54e4f00e8
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2025-03-31 19:53:15 +0200

    refactor: simplify ThanksMessage component styling

--- components/ThanksMessage.tsx
+++ components/ThanksMessage.tsx
@@ -4,11 +4,7 @@ import React from 'react'
 const ThanksMessage = () => {
   return (
     <Box sx={{
-        backgroundColor: "#232A34",
-        padding: "2rem",
-        width: "25.75rem",
-        height: "26rem",
-        borderRadius: "1.875rem",
+        
     }}>
       Thanks for your feedback!
   
