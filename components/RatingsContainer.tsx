10aeca2bf4518c05fc59e197de2927e345c75cc2
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2025-03-31 19:46:40 +0200

    feat: add feedback submission functionality with ThanksMessage component

--- components/RatingsContainer.tsx
+++ components/RatingsContainer.tsx
@@ -1,72 +1,82 @@
+'use client'
 import { Box, Button, Typography } from '@mui/material'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import RatingsButtons from './RatingsButtons'
+import ThanksMessage from './ThanksMessage'
 
 const RatingsContainer = () => {
-  return (
-    <Box sx={{
-        backgroundColor: "#232A34",
-        padding: "2rem",
-        width: "25.75rem",
-        height: "26rem",
-        borderRadius: "1.875rem",
-    }}>
+    const [submitted, setSubmitted] = useState(false);
+
+    return (
         <Box sx={{
-            backgroundColor: "#262E38",
-            borderRadius:"50%",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "3rem",
-            width: "3rem",
-            marginBottom: "1.875rem"
+            backgroundColor: "#232A34",
+            padding: "2rem",
+            width: "25.75rem",
+            height: "26rem",
+            borderRadius: "1.875rem",
         }}>
-            <Image 
-                src="images\icon-star.svg"
-                alt='star logo'
-                height={16}
-                width={16}
-            />
+            {submitted ? ( 
+                <ThanksMessage /> 
+            ) : ( 
+                <> 
+                    <Box sx={{
+                        backgroundColor: "#262E38",
+                        borderRadius:"50%",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        height: "3rem",
+                        width: "3rem",
+                        marginBottom: "1.875rem"
+                    }}>
+                        <Image 
+                            src="/images/icon-star.svg" // FIXED IMAGE PATH
+                            alt="star logo"
+                            height={16}
+                            width={16}
+                        />
+                    </Box>
 
-            
+                    <Typography variant="h2" sx={{
+                        fontSize: "2rem",
+                        fontWeight: 600,
+                        color: "#FFFFFF",
+                    }}>
+                        How did we do?
+                    </Typography>
+
+                    <Typography variant="body1" sx={{
+                        color: "#969FAD",
+                        margin: "1.5rem 0"
+                    }}>
+                        Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!
+                    </Typography>
+
+                    <RatingsButtons/>
+
+                    <Button sx={{
+                        backgroundColor: "#FC7614",
+                        color: "#131518",
+                        fontWeight: 600,
+                        borderRadius: "1.4063rem",
+                        fontSize: "1.125rem",
+                        width: "100%",
+                        height: "3rem",
+                        marginTop: "2rem",
+                        padding: "0.75rem",
+                        "&:hover": {
+                            backgroundColor: "#FFFFFF"
+                        },
+                    }}
+                    onClick={() => setSubmitted(true)}
+                    >
+                        Submit
+                    </Button> 
+                </>
+            )}
         </Box>
-        <Typography variant='h2' sx={{
-            fontSize: "2rem",
-            fontWeight: 600,
-            color: "#FFFFFF",
-            
-        }}>
-            How did we do?                
-        </Typography>
-        
-        <Typography variant='body1' sx={{
-            color: "#969FAD",
-            margin: "1.5rem 0"
-        }}>
-            Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!
-        </Typography>
-
-        <RatingsButtons/>
-
-        <Button sx={{
-            backgroundColor: "#FC7614",
-            color: "#131518",
-            fontWeight: 600,
-            borderRadius: "1.4063rem",
-            fontSize: "1.125rem",
-            width: "100%",
-            height: "3rem",
-            marginTop: "2rem",
-            padding: "0.75rem",
-            "&:hover": {
-                backgroundColor: "#FFFFFF"
-            },
-        }}>
-            Submit
-        </Button>
-    </Box>
-  )
+    );
 }
 
-export default RatingsContainer
+export default RatingsContainer;
---
+++ components/ThanksMessage.tsx
@@ -0,0 +1,19 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+const ThanksMessage = () => {
+  return (
+    <Box sx={{
+        backgroundColor: "#232A34",
+        padding: "2rem",
+        width: "25.75rem",
+        height: "26rem",
+        borderRadius: "1.875rem",
+    }}>
+      Thanks for your feedback!
+  
+    </Box>
+  )
+}
+
+export default ThanksMessage
